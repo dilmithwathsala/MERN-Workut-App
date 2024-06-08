bb4388036fde482556f1252a3cb0303a1b289942
dilmithwathsala <dilmithwathsala519@gmail.com>
2024-06-09 00:10:48 +0530

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,37 @@ require("dotenv").config();
 
 
 const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 //express app
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-    res.send("Home Page");
+
+// middleware
+app.use(express.json());   // to accept json data from body 
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 })
 
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {    
+    // Listen to port
+    app.listen(process.env.PORT, () => {
+        console.log(`connected to db & listening on port`, process.env.PORT)
+    })    
+})
+    .catch((err) => {
+        console.log(err);
+    })
 
-// Listen to port
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port`, process.env.PORT)
-});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+
+
 
 
