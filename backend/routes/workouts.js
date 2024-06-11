e5b55e84f218002ab2bb290cd68567dc5ad6d948
dilmithwathsala <dilmithwathsala519@gmail.com>
2024-06-11 19:48:39 +0530

    Created workout model

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const router = express.Router();
-
+const Workout = require("../models/workoutModel");
 
 // GET ALL workouts
 router.get("/", (req, res) => {
     res.json({message: "Get All workoutes"})
 })
 
-
 // GET SINGLE workout
-router.get("/:id", (req, res) => {
-    res.json({message: `Get Single workout with ID ${req.params.id}`})
+router.get('/:id', (req, res) => {
+    res.json({message: `Get Single workout`})
 })
 
+// POST a new workout
+router.post('/', async (req, res) => {
+    const{title, load, reps} = req.body
 
-// POST new workout
-router.post("/", (req, res) => {
-    res.json({message: "Create new workout"})
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
+
 
 // DELETE workout
 router.delete("/:id", (req, res) => {
