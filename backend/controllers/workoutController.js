516413cb7571713b8c9e434ca8416bb7a54dbe64
dilmithwathsala <dilmithwathsala519@gmail.com>
2024-06-12 03:04:34 +0530

    changes of single workout function

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,25 +1,39 @@
 const Workout = require("../models/workoutModel");
 
 //get all workouts
+const getWorkouts = async (req, res) => {
+    const workoutes = await Workout.find({}).sort({createdAt: -1});
 
+    res.status(200).json(workoutes);
+}
 
 
 
 //get a single workout
+const getWorkout = async (req, res) => {
+    const { id } = req.params;
 
+    const workout = await Workout.findById(id)
+
+    if(!workout) {
+        return res.status(400).json({error: "No such workout"})
+    }
+
+    res.status(200).json(workout)
+}
 
 
 
 //create a new workout
 const createWorkout = async (req, res) => {
-   const {title, load, reps} = req.body
+    const{title, load, reps} = req.body
 
-   try{
-    const workout = await Workout.create({title, load, reps})
-    res.status(200).json(workout)
-   }catch(error){
-    res.status(400).json({error: error.message})
-   }
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 
@@ -36,7 +50,6 @@ const createWorkout = async (req, res) => {
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout,
-    deleteWorkout,
-    updateWorkout
+    createWorkout
+   
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,38 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+} = require("../controllers/workoutController");
 
 // GET ALL workouts
-router.get("/", (req, res) => {
-    res.json({message: "Get All workoutes"})
-})
+router.get('/', getWorkouts)
 
 // GET SINGLE workout
-router.get('/:id', (req, res) => {
-    res.json({message: `Get Single workout`})
-})
+router.get('/:id', getWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const{title, load, reps} = req.body
-
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
-
+router.post('/', createWorkout)
 
 // DELETE workout
 router.delete("/:id", (req, res) => {
-    res.json({message: `Delete workout with ID ${req.params.id}`})
+    res.json({message: `Delete workout with ID`})
 })
 
 
 // UPDATE workout
 router.patch("/:id", (req, res) => {
-    res.json({message: `Update workout with ID ${req.params.id}`})  
+    res.json({message: `Update workout with ID`})  
 })
 
 
