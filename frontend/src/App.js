eba40f6974a6d199256f6164579329676ce16a59
dilmithwathsala <dilmithwathsala519@gmail.com>
2024-06-14 23:47:08 +0530

    updated app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 
 
 function App() {
