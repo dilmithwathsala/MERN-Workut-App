9aa66d105f7297c9f65993a9c88f698cd8c8475d
dilmithwathsala <dilmithwathsala519@gmail.com>
2024-06-14 18:55:42 +0530

    added proxy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
 
     return (
         <div className="home">
-            <h1>Home</h1>
+            <div className="workouts">
+            {workouts && workouts.map(workout => (
+                <div key={workout.id}>{workout.title}</div>
+            ))}
+            </div>
         </div>
-    )
+      );
 }
 
 export default Home
